8ac67507c7dc22794de0efc15e02445f3e2b0c6e
Shashank Taliwal <72131779+shashank-taliwal@users.noreply.github.com>
2022-11-15 23:00:33 -0800

    Time: 1244 ms (7.08%), Space: 283.1 MB (9.15%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -2,33 +2,43 @@ class Solution {
 public:
     string longestPalindrome(string s) {
         int n=s.length();
-        vector<vector<bool>> dp(n,vector<bool>(n,false));
+        vector<vector<int>> dp(n,vector<int>(n,0));
+        // dp[i][j] indicates the s(i......j) is a palindrome
+        // Conditions to consider:-
+        //     1) if i equals j, dp[i][j]=1
+        //     2) if |i-j| equals 1 and s(i) equals s(j), dp[i][j]=1
+        //     3) if s(i) equals s(j) and s(i+1................j-1) is palindrome, dp[i][j]=1
+        
+        // Base case:- if (i>j) execution stops
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<n;j++){
+        //         cout<<dp[i][j]<<" ";
+        //     }
+        //     cout<<endl;
+        // }
+        int left=-1,right=-1;
+        int max_length=0;
+        string ans;
         for(int i=n-1;i>=0;i--){
             for(int j=0;j<n;j++){
                 if(i<=j){
                     if(i==j){
-                        dp[i][j]=true;
+                        dp[i][j]=1;
                     }
-                    else if(j-i==1 and s[i]==s[j]){
-                        dp[i][j]=true;
+                    else if(abs(i-j)==1 and s[i]==s[j]){
+                        dp[i][j]=1;
                     }
-                    else{
-                        if(dp[i+1][j-1] and s[i]==s[j]){
-                            dp[i][j]=true;
-                        }
+                    else if(s[i]==s[j]){
+                        dp[i][j]=dp[i+1][j-1];
                     }
+                }
+                if(dp[i][j]==1 and j-i+1>max_length){
+                    max_length=j-i+1;
+                    left=i;
+                    right=j;
                 }
             }
         }
-        int left=0,right=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(dp[i][j] and j-i>right-left){
-                    left=i,right=j;
-                }
-            }
-        }
-        string ans;
         for(int i=left;i<=right;i++){
             ans+=s[i];
         }
