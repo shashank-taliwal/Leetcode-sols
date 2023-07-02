896030389d28c1ae7cc3e2ba7ce99669e95d6eb1
Shashank Taliwal <72131779+shashank-taliwal@users.noreply.github.com>
2023-07-02 08:44:31 +0530

    Time: 41 ms (66.05%), Space: 19.1 MB (21.03%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,41 +1,47 @@
 class Solution {
+    private:
     bool isValid(int x, int y, int rows, int cols){
-        if(x<0 or x>=rows or y<0 or y>=cols){
+        if(x<0 or x>=rows or y>=cols or y<0){
             return false;
         }
         return true;
     }
-    void dfs(vector<vector<char>>& grid, vector<vector<int>>& visited, int x, int y){
-        visited[x][y]=1;
+    void bfs(vector<vector<char>>& grid, vector<vector<int>>& visited, int x, int y){
+        queue<pair<int,int>> q;
         int rows=grid.size();
         int cols=grid[0].size();
         int dx[4]={1,-1,0,0};
         int dy[4]={0,0,1,-1};
-        
-        for(int i=0;i<4;i++){
-            int x0=x+dx[i];
-            int y0=y+dy[i];
-            if(isValid(x0,y0,rows,cols) and visited[x0][y0]==0 and grid[x0][y0]=='1'){
-                dfs(grid,visited,x0,y0);
+        q.push({x,y});
+        visited[x][y]=1;
+        while(!q.empty()){
+            int x0=q.front().first;
+            int y0=q.front().second;
+            q.pop();
+            for(int i=0;i<4;i++){
+                int x1=x0+dx[i];
+                int y1=y0+dy[i];
+                
+                if(isValid(x1,y1,rows,cols) and grid[x1][y1]=='1' and !visited[x1][y1]){
+                    q.push({x1,y1});
+                    visited[x1][y1]=true;
+                }
             }
         }
+        
     }
 public:
     int numIslands(vector<vector<char>>& grid) {
-        // The question is about finding number of connected components
-        
-        // Approach1 DFS
-        
+        //Approach 2 BFS
         int rows=grid.size();
         int cols=grid[0].size();
-        
         vector<vector<int>> visited(rows,vector<int>(cols,0));
         int cc=0;
         for(int i=0;i<rows;i++){
             for(int j=0;j<cols;j++){
-                if(grid[i][j]=='1' and visited[i][j]==0 and isValid(i,j,rows,cols)){
+                if(grid[i][j]=='1' and visited[i][j]==0){
                     cc++;
-                    dfs(grid,visited,i,j);
+                    bfs(grid,visited,i,j);
                 }
             }
         }
