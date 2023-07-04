d7a8bcb2324bf279dd2bf2e91fdcc496f480b3d5
Shashank Taliwal <72131779+shashank-taliwal@users.noreply.github.com>
2023-07-04 17:39:05 +0530

    Time: 309 ms (6.60%), Space: 69.3 MB (5.26%) - LeetHub

---
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+      // Approach: Using topological sort (kahn's algorithm to find the cycle in a graph)
+        // The question boils down to finding the nodes that do not lead to cycle of the graph
+        
+        int n=graph.size();
+        map<int,vector<int>> adj;
+        vector<int> outDegree(n,0);
+        // for(vector<int> node: graph){
+        //     for(int neighs: node){
+        //         outDegree[neighs]++;
+        //     }
+        // }
+        for(int i=0;i<n;i++){
+            outDegree[i]+=graph[i].size();
+            for(int j=0;j<graph[i].size();j++){
+                adj[graph[i][j]].push_back(i);
+            }
+        }
+        vector<int> ans;
+        queue<int> q;
+        for(int i=0;i<n;i++){
+            if(outDegree[i]==0){
+                q.push(i);
+            }
+        }
+        
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            ans.push_back(node);
+            
+            for(int neighs:adj[node]){
+                outDegree[neighs]--;
+                if(outDegree[neighs]==0){
+                    q.push(neighs);
+                }
+            }
+        }
+        sort(ans.begin(),ans.end());
+        return ans;
+    }
+};
